ebb2c97b0124b7ee77dcefbccaf38a7040f6bc58
Jackweb-rgba <radohery09@gmail.com>
2024-12-14 06:57:09 +0100

    initialisation corps de ui

--- gestion-bibliotheque/src/app/app.component.ts
+++ gestion-bibliotheque/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { SingleBookComponent } from './single-book/single-book.component';
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    SingleBookComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
